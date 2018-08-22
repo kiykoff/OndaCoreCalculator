21e9730ba506af503bf9923875445e4c649a7f23
a.kiykoff <a.kiykoff@gmail.com>
2018-08-22 17:23:22 +0300

    Добавьте файлы проекта.

---
+++ OndaCoreWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OndaCoreWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
